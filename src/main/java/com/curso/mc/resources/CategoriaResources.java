593703922b93543275f1ddde6d4d7de5d67079ef
jonas <jns.klr@gmail.com>
2019-03-29 19:29:23 -0300

    Categoria domain

--- src/main/java/com/curso/mc/resources/CategoriaResources.java
+++ src/main/java/com/curso/mc/resources/CategoriaResources.java
@@ -1,16 +1,31 @@
 package com.curso.mc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.mc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResources {
-	
+
 	@GetMapping
-	public String listar() {
-		return "REST está funcioanndo";
+	public List<Categoria> listar() {
+
+		Categoria c1 = new Categoria(1, "Info");
+
+		Categoria c2 = new Categoria(2, "Home");
+
+		List<Categoria> lista = new ArrayList<>();
+
+		lista.addAll(Arrays.asList(c1, c2));
+
+		return lista;
 	}
 
 }
